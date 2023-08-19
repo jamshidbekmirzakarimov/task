0451dca935747f1b899a0c802a1be59b4eb70eb3
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-08-19 16:49:00 +0500

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="w-[100%] max-w-[800px] mx-auto">
+          
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
